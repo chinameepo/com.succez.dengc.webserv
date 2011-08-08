576997d7950963922e4aa1b47a2d9b9876535958
deng chao <deng.369@gmail.com>
2011-08-08 16:39:21 +0800

    重新放到另外一个仓库里面

---
+++ server1/src/main/java/com/succez/dengc/serv/Response.java
@@ -0,0 +1,276 @@
+package com.succez.dengc.serv;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *All right resrvered esensoft(2011)
+ * @author  邓超   deng.369@gmail.com
+ * @version 1.0,创建时间：2011-8-2 下午07:12:36
+ * @since   jdk1.5
+ * 浏览器应答类，通过获得的socket对象，利用socket对象的IO流和浏览器通信。
+ */
+public class Response implements Runnable {
+	/**
+	 * 这个socket对象必须要从Serve监听对象传过来，和浏览器交互的IO流都是通过它来建立，
+	 * 通过对象的输入流来获取url，通过对象的输出流来向浏览器返回内容。 不可缺少
+	 */
+	private Socket socket;
+	/**
+	 *  服务器的默认根目录，从Serve对象中获得 ，可以缺省
+	 * */
+	private String root;
+    private Logger logger = LoggerFactory.getLogger(Response.class);
+   
+	public Response(Socket response, String root) {
+		this.socket = response;
+		this.root = root;
+	} 
+	/**
+	 * C:系统盘，最好不要将根目录设在这里
+	 * */
+	public Response(Socket response) {
+		this(response, "c:\\");
+	}
+	
+	/**
+	 * 核心方法，规定该线程的执行内容，通过浏览器的输入流得到请求报文头，从中截取文件Url,
+	 * 如果文件存在，读取文件，将内容传输到浏览器。不存在，返回404文件
+	 * 
+	 * @exception IOException
+	 * */
+	public void run() {
+			OutputStream out = null;
+			InputStream socketiStream = null;
+			try {
+				socketiStream = socket.getInputStream();
+				String httpRequestHead = getHttpHead(socketiStream);
+				String sourcePath = root+getUrl(httpRequestHead);;
+				try {
+					out = socket.getOutputStream();
+					fileToBrowser(sourcePath, out);
+				} catch (IOException e) {
+					logger.error("浏览器输出流对象新建的时候出错！程序终止！来自方法：【run】");
+					return;
+				}
+			} catch (IOException e) {
+				logger.error("程序运行中出现错误，程序终止！来自方法：【run】");
+				return;
+			} finally {
+				try {
+					out.close();
+					socketiStream.close();
+					socket.close();
+				} catch (IOException e) {
+					logger.error("文件流在关闭的时候出现错误！不能正常关闭。自方法：【run】");
+					return;
+				}
+			}
+	}
+	/**
+	 * 获取请求报文所有内容。
+	 * @param inputStream
+	 * @return String
+	 */
+	public String getHttpHead(InputStream inputStream) {
+		if(inputStream==null)
+			return "";
+		StringBuilder requestStr = new StringBuilder();
+		try {
+			int len=-1;
+			byte[] buff =new byte[1024];
+			do{
+				len=inputStream.read(buff);
+				if(len!=-1)
+				{
+					requestStr.append(new String(buff,0,len,"UTF-8")).append('\n');
+				}
+			}while(len==1024);
+		} catch (IOException e) {
+			logger.error("读取请求报文过程中出错！来自方法：【 getHttpHead】");
+			return "";
+		}
+		logger.info("请求的报文头是：/n{}",requestStr.toString());
+		return requestStr.toString();
+	}
+	/**
+	 * @param requestString
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	public String getUrl(String requestString)throws UnsupportedEncodingException {
+		if ("".equals(requestString))
+			return "";
+		/* 截断报文，获取url。请求报文第一行的格式都是 GET /urlcontent**** HTTP/1.X*/
+		String url  = requestString.substring(5,requestString.lastIndexOf("HTTP/1.")).trim();
+		/* 如果对方输入的页面是空的，例如http://localhost:8080/,跳至首页 */
+		if ("".equals(url))
+		{
+			url = "index.html";
+			return url;
+		}
+		/*浏览器处理url要用到UTF-8编码，处理空格后会变成“%20”或者“+”,要进行解码还原*/
+		url =URLDecoder.decode(url,"UTF-8");
+		logger.info("文件名是：{}", url);
+		return url;
+	}
+	
+	/**
+	 * 读取文件，写入浏览器。 如果url指定的文件存在，传到浏览器。如果不存在，就在浏览器上显示404页面
+	 * 这里可能出现的问题：如果用户指定的404页面不存在， 那么程序就会陷入死循环。
+	 * InputStream会抛出FileNotFoundException异常，is.read(buf)会抛出IOException
+	 * @param sourceName
+	 *            ，要读取文件的文件名
+	 * @param out
+	 *            将文件写入浏览器的输出流
+	 * */
+	public void fileToBrowser(String sourceName, OutputStream out) {
+		if(out==null)
+			return;
+		File file = new File(sourceName);
+		if (file.exists()&&file.isFile()) {
+			sendHead(sourceName, out);
+			sendFile(file,out);
+		} else {
+			logger.error("程序要找的文件【{}】找磁盘上找不到！将会用404页面来替代这个文件返回！来自方法：【fileToBrowser】", sourceName);
+             /* 注意这是递归调用，如果说这个404文件不存在，就会陷入死循环*/
+			fileToBrowser(root+"404.html", out);
+		}
+		}
+	
+	/**
+	 * 发送报文头到一个ouputStrem对象，就是socket的outputStream对象
+	 * 这代码这么写的前提是：参数都是有意义的，都不为空。
+	 * @param sourceName
+	 * @param out
+	 * @param file
+	 * @throws IOException
+	 */
+	public void sendHead(String sourceName, OutputStream out){	
+		String head = "HTTP/1.1 200 OK" + "\n"
+				+ "Date: Thu, 21 Jul 2011 01:45:42 GMT" + "\n"
+				+ "Content-Length: " + (new File(sourceName)).length() + "\n"
+				+ "Content-Type: "+getMIMEtype(sourceName) + "\n"
+				+ "Cache-Control: private" + "\n"
+				+ "Expires: Thu, 21 Jul 2011 01:45:42 GMT" + "\n"
+				+ "Connection: Keep-Alive" + "\n" + "\n";
+		try {
+			out.write(head.getBytes());
+		} catch (IOException e) {
+			logger.error("传送文件{}前，发送报文头失败！来自方法【sendHead】",sourceName);
+			return;
+		}
+	}
+
+	/**
+	 * 发送文件到一个ouputStrem对象，就是socket的outputStream对象
+	 * 这代码这么写的前提是：这三个参数都是有意义的，都不为空。
+	 * @param out
+	 * @param file
+	 */
+	public void sendFile( File file,OutputStream out) {
+		InputStream is =null;
+		try {
+			is = (InputStream)(new FileInputStream(file));
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = is.read(buf))!= -1) {
+				out.write(buf, 0, len);
+			}
+			out.flush();
+		} catch (IOException e) {
+			logger.error("文件【{}】读取、写入浏览器中出现错误！来自方法:【sendFile】",file.toString());
+			return;
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e2) {
+				logger.error("文件{}的读取流无法正常关闭！来自方法:【sendFile】",file.toString());
+				return;
+			}
+		}
+	}
+	
+	/**
+	 * 根据文件名的后缀类型来确定返回类型。因为执行这段代码之前， 已经知道这个文件肯定存在，所以肯定是有后缀的
+	 * 决定应答报文头“Content-Type”那一行的内容， 例如如果是aa.jpg，返回就是"Content-Type: image/jpeg"
+	 * 要注意，文件名中包含了“”/\：*<>等符号都是不合法的。这个里面的判断比较多。
+	 * @return String
+	 * @param String sourceString ，url的字符串
+	 *            
+	 */
+	public String getMIMEtype(String sourceString) {
+		if(sourceString==null||"".equals(sourceString))
+			return "text/html;charset=gb2312";
+		/*先把句号的位置取出来，是为了防止sourceString中没有“.”号，返回-1，
+		 * 从而造成substring值溢出的情况发生。*/
+		int indexOfDot =sourceString.lastIndexOf('.');
+		if(indexOfDot==-1)
+			return "text/html;charset=gb2312";
+		String type=sourceString.substring(indexOfDot);
+		Map<String, String> map= new HashMap<String, String>();
+		initMap("src/main/java/com/succez/dengc/serv/mimeMap",map);
+		String returnType =map.get(type);
+		if(returnType==null)
+			return "text/html;charset=gb2312";
+		return returnType;
+	}
+	/**
+	 *对map对象进行初始化，把一个文件里面的hash关系读取，放入map中
+	 *@param String sourcFile map关系保存的文件所在     ' '  '.'  "_" '_' '-'
+     *@param Map<String,String> map存放读取出来的关系印射
+	 * */
+	public void initMap(String sourcFile,Map<String,String> map)
+	{   
+		BufferedReader in =null;
+		try {
+			in = new BufferedReader(new FileReader(sourcFile));
+			String[] mapString = new String[2];
+			String aline;
+			try {
+				do {
+				   aline =in.readLine();
+				   if(aline!=null)
+				   {  
+					   /*String.splite函数第二个参数是规定按照表达式分割n-1次，所以这里是按照==号分割一次
+					    * 当然也有可能管理员疏忽，没用加上==号，那么印射值就返回null，接着mime就
+					    * 返回text/html类型*/
+					   mapString =aline.split("==",2);
+					   map.put(mapString[0].trim(), mapString[1].trim());
+				   }
+				   } while (aline!=null);	
+			} catch (IOException e) {
+				logger.error("在读取map的印射关系的时候出错，文件名是{}.来自【initMap】",sourcFile);
+				return;
+			}	                           	
+		} catch (FileNotFoundException e) {
+			logger.error("map的印射关系文件名{}找不到，请检查您的路径是否正确。来自【initMap】",sourcFile);
+			return;
+		}finally
+		{
+			try {
+				in.close();
+			} catch (Exception e2) {
+				logger.error("在关闭map的印射关系的文件出错，文件名是{}.来自【initMap】",sourcFile);
+				return;
+			}
+		}
+	}
+}
+
+
---
+++ server1/src/test/java/com/succez/dengc/serv/TestResponse.java
@@ -0,0 +1,295 @@
+package com.succez.dengc.serv;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.succez.dengc.serv.Response;;
+
+/**
+ * All right resrvered esensoft(2011)
+ * 
+ * @author 邓超 deng.369@gmail.com
+ * @version 1.0,创建时间：2011-8-3 上午08:44:10
+ * @since jdk1.5 
+ * 对response类的测试类，请注意。如果你要用自己的文件拖进来做测试，请做好备份。
+ */
+public class TestResponse {
+	
+	
+	
+	/**
+	 * 具体的写入头文件、写入文件的，这个被测试方法的子方法已经测试过了
+	 * 这个类主要是测试：1，错误的地址是否会返回404页面。2，应答报文头是否写进了文件
+	 * 先使用bufferedreader的realine方法出报文头，再接着比较以后的内容。
+	 * 不管是文本文件，还是图片，一视同仁
+	 * @throws IOException
+	 * */
+	@Test
+	public final void testFileToBrowser() throws IOException {
+		testFileToBroSub("src/test/java/testfl/test.txt","src/test/java/generfl/test-all.txt");
+		testFileToBroSub("src/test/java/testfl/test.html", "src/test/java/generfl/test-all.html");
+		testFileToBroSub("src/test/java/testfl/aa.png", "src/test/java/generfl/aa-all.png");
+		testFileToBroSub("src/test/java/testfl/bb.jpg", "src/test/java/generfl/bb-all.jpg");
+		/*测试输入错误url被替换为404页面，有点难度，不过还是可以实现的。
+		 *下面我先给个错误地址，不出意外，err.html就是c://4041.html加上报文头*/
+		testFileToBroSub("fewfweij.fwe", "src/test/java/generfl/err.html");
+		/*然后我让404页面也写入另外的文件*/
+		testFileToBroSub("c://404.html", "src/test/java/generfl/404-all.html");
+		/*接着对比两个写入的结果*/
+		assertEquals(readFile("src/test/java/generfl/err.html"), readFile("src/test/java/generfl/404-all.html"));
+	}
+	/**
+	 * 为了测试FileToBrowser(String source,OutputStream out)写的辅助测试方法
+	 * 验证写入的报文头是否正确
+	 * */
+	public void checkHead(String source,BufferedReader resultFileReader,Response response )
+	throws IOException{
+		File file= new File(source);
+		// 验证报文头是否正确
+		assertEquals("HTTP/1.1 200 OK", resultFileReader.readLine());
+		assertEquals("Date: Thu, 21 Jul 2011 01:45:42 GMT", resultFileReader.readLine());
+		assertEquals("Content-Length: " + file.length(), resultFileReader.readLine());
+		assertEquals("Content-Type: " + response.getMIMEtype(source),resultFileReader.readLine());
+		assertEquals("Cache-Control: private", resultFileReader.readLine());
+		assertEquals("Expires: Thu, 21 Jul 2011 01:45:42 GMT",resultFileReader.readLine());
+		assertEquals("Connection: Keep-Alive", resultFileReader.readLine());
+		assertEquals("", resultFileReader.readLine());
+	}
+	/**
+	 * 为了测试FileToBrowser(String source,OutputStream out)写的辅助测试方法
+	 * 验证从一个文件写入另一个文件的内容是否正确
+	 * @param resultReader
+	 * @param sourceReader
+	 * @throws IOException
+	 */
+	public void checkConten(BufferedReader resultReader,BufferedReader sourceReader)
+			 throws IOException {
+		String sourceString ,outString;
+		while (((sourceString = sourceReader.readLine()) != null)
+				&&((outString = resultReader.readLine()) != null)) {
+			assertEquals(sourceString, outString);
+		}
+	}
+	/**
+	 * 辅助测试fileToBrowser的方法，验证写入的报文头+内容是否正确。测试方法中，
+	 * 我们只是需要传入两个文件的路径就可以完成测试。此处注意的是测试的文件c://404.html，
+	 * 这个文件比较特殊，如果把它放入了工程目录下，文件会变大，导致测试部准确。所以必须
+	 * 是测试它的原文件。
+	 * @throws IOException
+	 * */
+	public void testFileToBroSub(String sourceName,String outPath) throws IOException
+	{
+		Response response = new Response(null);
+		OutputStream out = null;
+		BufferedReader resultFileReader = null;
+		BufferedReader sourceFileReader = null;	
+		File sourceFile = new File(sourceName);
+		if(!sourceFile.exists()||!sourceFile.isFile())
+			{
+			sourceFile = new File("c://404.html");
+			sourceName ="c://404.html";
+			}
+		try {
+			out = (OutputStream)(new FileOutputStream(outPath));
+			response.fileToBrowser(sourceName, out);
+			sourceFileReader = new BufferedReader(new FileReader(sourceName));
+			resultFileReader = new BufferedReader(new FileReader(outPath));
+			checkHead(sourceName, resultFileReader, response);
+			checkConten(resultFileReader, sourceFileReader);
+		} finally {
+			try {
+				out.close();
+				resultFileReader.close();
+				sourceFileReader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	/**
+	 * 确定这个被测试的方法的参数out,对象肯定不为空.url,file都是不为空的。查看发送的报文头是否正确！
+	 * 
+	 * @throws IOException
+	 * */
+	@Test
+	public final void testSendHead() throws IOException {
+		Response response = new Response(null);
+		testSendHeadSub("src/test/java/testfl/test.txt","src/test/java/generfl/test-head.txt", response);
+		testSendHeadSub("src/test/java/testfl/test.txt","src/test/java/generfl/test-head.txt",response);
+		testSendHeadSub("src/test/java/testfl/test.html","src/test/java/generfl/test-head.txt",response);
+		testSendHeadSub("src/test/java/testfl/aa.png", "src/test/java/generfl/aa-head.txt",response);
+		testSendHeadSub("src/test/java/testfl/bb.jpg", "src/test/java/generfl/bb-head.txt",response);
+	}
+	/**
+	 * 这个函数大部分功能就是函数checkHead（）的功能，这是因为参数不同才特意新建了函数。
+	 * */
+	public void testSendHeadSub(String sourceName,String outPath,Response response) throws IOException
+	{
+		OutputStream out = null;
+		BufferedReader resultFileReader = null;
+		//查看文本文件
+		try {
+			out = (OutputStream) (new FileOutputStream(outPath));
+			response.sendHead(sourceName , out);
+			resultFileReader = new BufferedReader(new FileReader(outPath));
+			checkHead(sourceName, resultFileReader, response);
+		} finally {
+			try {
+				out.close();
+				resultFileReader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 我认为即便不是文本文档，也可以用字符串来对文件的内容进行比较 前面，待测试的方法已经验证，这个函数的out肯定是不为空的
+	 * 
+	 * @throws IOException
+	 * */
+	@Test
+	public final void testSendFile() throws IOException {
+		testSendFileSub("src/test/java/testfl/test.txt","src/test/java/generfl/test-result.txt");
+		testSendFileSub("src/test/java/testfl/aa.png","src/test/java/generfl/aa-result.png");
+		testSendFileSub("src/test/java/testfl/bb.jpg","src/test/java/testfl/bb-result.jpg");
+		testSendFileSub("src/test/java/testfl/404.html","src/test/java/testfl/404-result.html");
+		testSendFileSub("src/test/java/testfl/MAP.doc","src/test/java/testfl/MAP-result.doc");
+	}
+	/**
+	 * 为了测试SendFile()写的辅助函数，
+	 * 查看写入的文件是否很原来的文件内容一样*/
+	public void testSendFileSub(String sourceName,String outPath) throws IOException
+	{
+		Response response = new Response(null);
+		OutputStream out = null;
+		try {
+			out = (OutputStream)(new FileOutputStream(outPath));	
+			File file = new File(sourceName);
+			response.sendFile(file, out);
+			assertEquals(readFile(sourceName),readFile(outPath));
+		} finally {
+			try {
+				out.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}	
+	}
+	
+	/**
+	 * GetMIMEtpye()的测试方法，目前不能解决的问题是，如果用户输入url中就包含了很多“”号，或者/n/t之类的转义字符 如何处理？
+	 */
+	@Test
+	public final void testGetMIMEtpye() {
+		Response response = new Response(null);
+		// 图片类型
+		assertEquals("image/jpeg", response.getMIMEtype("a!@#$!%^&*%HRa.jpg"));
+		assertEquals("image/jpeg",response.getMIMEtype("a$&*@;';&##^@a.jpg"));
+		assertEquals("image/jpeg",response.getMIMEtype("xW@@xFE$--=-=YYW%Q.jpeg"));		
+		assertEquals("image/png",response.getMIMEtype("?pf%%%efw jo.png"));	
+		assertEquals("image/gif",response.getMIMEtype("i@@22-=t4-+-5+  -.gif"));
+		// 文字类型
+		assertEquals("text/xml",response.getMIMEtype("!!@!@$FEAAEH　　.xml"));
+		assertEquals("text/plain", response.getMIMEtype("sss....txt"));
+		assertEquals("text/html;charset=gb2312",response.getMIMEtype("s. .t  x  t"));
+		assertEquals("text/html;charset=gb2312",response.getMIMEtype("Test. j av  a "));
+		assertEquals("text/plain", response.getMIMEtype("c s s s s---ss....c"));
+		assertEquals("text/plain",response.getMIMEtype("fff ff fw@#$.cpp"));		
+		assertEquals("text/plain", response.getMIMEtype("Test.java"));
+		// 在后面留一些空格
+		assertEquals("text/html;charset=gb2312",response.getMIMEtype("test.java  "));
+		assertEquals("text/html;charset=gb2312", response.getMIMEtype("inde x .h "));
+		// 在前面留一些空格
+		assertEquals("text/plain",response.getMIMEtype("  Test.java"));
+		assertEquals("text/plain", response.getMIMEtype("cpp_h)(942334).txt"));
+		assertEquals("text/plain", response.getMIMEtype("index.h"));
+		assertEquals("text/html;charset=gb2312",response.getMIMEtype("index.html"));
+		assertEquals("text/html;charset=gb2312",response.getMIMEtype("index.htm"));	
+		assertEquals("text/html;charset=gb2312",response.getMIMEtype("index. ht m l"));	
+		// 空类型
+		assertEquals("text/html;charset=gb2312", response.getMIMEtype(""));
+		assertEquals("text/html;charset=gb2312", response.getMIMEtype("abc"));
+		assertEquals("text/html;charset=gb2312", response.getMIMEtype(null));
+	}
+	
+	/**
+	 * 为了测试方法服务的方法，读取一个文件的内容，不管是文件还是图片，我们都用字符串返回 。
+	 * 不设置成同步方法的话 ，文件可能会在读取过程中损坏
+	 * 
+	 * @param String
+	 *            filePath 文件的路径
+	 */
+	public synchronized String readFile(String filePath) {
+		InputStream in = null;
+		StringBuilder builder = new StringBuilder();
+		File file = new File(filePath);
+		if (file.exists() && file.isFile()) {
+			try {
+				in = (InputStream) (new FileInputStream(file));
+				int len;
+				byte[] buffer = new byte[512];
+				while ((len = in.read(buffer)) != -1) {
+					builder.append(new String(buffer, 0, len, "UTF-8"));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					in.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+		}
+		return builder.toString();
+	}
+	/**
+	 * InitMap的测试方法
+	 * */
+	@Test
+	public final void testInitMap()
+	{
+		Response response = new Response(null);
+		String sourceFile ="src/main/java/com/succez/dengc/serv/mimeMap";
+		Map<String, String> map = new HashMap<String, String>();
+		response.initMap(sourceFile, map);
+		assertEquals("text/html;charset=gb2312",map.get(".html"));
+		assertEquals("text/html;charset=gb2312",map.get(".htm"));
+		assertEquals("text/xml",map.get(".xml"));
+		assertEquals("text/plain",map.get(".txt"));
+		assertEquals("text/plain",map.get(".txt"));
+		assertEquals("text/plain",map.get(".txt"));
+		assertEquals("text/plain",map.get(".txt"));
+		assertEquals("text/plain",map.get(".c"));
+		assertEquals("text/plain",map.get(".cpp"));
+		assertEquals("text/plain",map.get(".java"));
+		assertEquals("text/plain",map.get(".pl"));
+		assertEquals("text/plain",map.get(".cc"));
+		assertEquals("text/plain",map.get(".h"));
+		assertEquals("text/css",map.get(".css"));
+		assertEquals("image/gif",map.get(".gif"));
+		assertEquals("image/png",map.get(".png"));
+		assertEquals("image/jpeg",map.get(".jpeg"));
+		assertEquals("image/jpeg",map.get(".jpg"));
+		
+		assertNull(map.get(""));
+		assertNull(map.get(null));
+		assertNull(map.get("xienigewnifwjigw"));
+		assertNull(map.get(".abc"));
+		assertNull(map.get(".TXt"));
+	}
+}
